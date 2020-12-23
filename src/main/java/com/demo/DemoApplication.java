3ee09d768fa084f4b12c36d507a8b6da4df4fbf2
EDZ <ycqzbdwg@163.com>
2020-12-23 17:38:48 +0800

    security

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -3,7 +3,11 @@ package com.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 @MapperScan(basePackages = {"com.demo.mapper"})
 public class DemoApplication {
--- src/test/java/com/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/DemoApplicationTests.java
@@ -1,15 +1,40 @@
 package com.demo;
 
 
+import com.demo.pojo.SysUser;
+import com.demo.pojo.SysUserRole;
+import com.demo.service.SysUserRoleService;
+import com.demo.service.SysUserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    private SysUserService sysUserService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private SysUserRoleService sysUserRoleService;
+
     @Test
     void contextLoads() {
-
+        // 注册用户
+        SysUser sysUserEntity = new SysUser();
+        sysUserEntity.setUsername("wang");
+        sysUserEntity.setPassword(bCryptPasswordEncoder.encode("123456"));
+        // 设置用户状态
+        sysUserEntity.setStatus("NORMAL");
+        sysUserService.save(sysUserEntity);
+        // 分配角色 1:ADMIN 2:USER
+        SysUserRole sysUserRoleEntity = new SysUserRole();
+        sysUserRoleEntity.setRoleId(2L);
+        sysUserRoleEntity.setUserId(sysUserEntity.getUserId());
+        sysUserRoleService.save(sysUserRoleEntity);
+        System.out.println(sysUserEntity);
     }
 
 }
